d68a701d12f241fee2bf681decdd3770099fc5ae
mikeybacque <mikey.bacque@gmail.com>
2016-03-08 17:27:05 -0500

    add javascript

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var $header = $('header');
+var $h1 = $('h1');
+var $main = $('main');
+var $pfirst = $('main p:first-child');
+var $h2 = $('h2');
+var $mainlist = $('main ul');
+var $img = $('img');
+var $ulfirst = $('ul:first-child');
+var $ullast = $('ul:last-child');
+var $dl = $('dl');
+var $dt = $('dt');
+
+$header.addClass('masthead');
+
+$h1.removeClass('big-heading');
+$h1.addClass('logo');
+
+$main.addClass('wrapper');
+$pfirst.addClass('intro');
+
+$h2.addClass('slug-head');
+
+$mainlist.addClass('slug-list');
+
+$img.addClass('slug-img');
+
+$ulfirst.addClass('slug-list-first');
+
+$ullast.addClass('slug-list-last');
+
+$dl.addClass('classification');
+
+$dt.removeClass('big-label');
+
+$ullast.show(duration, complete, options, duration, easing, complete);
